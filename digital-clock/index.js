d3c507c37862d30fe9c9b55a496b6fcba25d6363
Jefiya123 <109594560+Jefiya123@users.noreply.github.com>
2022-08-17 23:29:39 +0530

    Add files via upload

---
+++ digital-clock/index.js
@@ -0,0 +1,28 @@
+const hour=document.getElementById("hour")
+const min=document.getElementById("minutes")
+const sec=document.getElementById("seconds")
+const ampm=document.getElementById("ampm")
+
+function clock() {
+    let h=new Date().getHours()
+    let m=new Date().getMinutes()
+    let s=new Date().getSeconds()
+    let ampm="AM"
+    if(h>12){
+        h=h-12
+        ampm="PM"
+    }
+    h=h<10?'0'+h:h
+    m=m<10?'0'+m:m
+    s=s<10?'0'+s:s
+
+    hour.innerText=h;
+    min.innerText=m;
+    sec.innerText=s;
+    ampm.innerText=ampm;
+    setTimeout(()=>{
+        clock()
+},1000)
+
+}
+clock()
